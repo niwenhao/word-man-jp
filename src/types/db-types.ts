403fe4f63946a3dd78ba3d5423c605bf21ffe206
niwenhao <niwenhao@gmail.com>
2025-02-12 16:33:20 +0900

    Add DB model

---
+++ src/types/db-types.ts
@@ -0,0 +1,16 @@
+export interface Chapter {
+  id: number;
+  name: string;
+  description?: string;
+  lastUpdate: Date;
+  questions: Question[];
+}
+
+export interface Question {
+  id: number;
+  question: string;
+  questionPronounced: string;
+  answer: string;
+  chapterId: number;
+  chapter: Chapter;
+}
